9ce890ffbf2c91ea5d2b0abeac8ba7edfaebb03f
nosrednawall <anderson.inacio@ufpr.br>
2024-09-29 15:17:34 -0300

    adicionado monitor virtual

--- .local/bin/check_second_monitor_is_active.sh
+++ .local/bin/check_second_monitor_is_active.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 monitor=$(xrandr | grep "HDMI-0 connected" -ci | cut -c 1-16 )
+virtual1=$(xrandr | grep "Virtual-1 connected" -ci | cut -c 1-16)
 
 if [ $monitor -eq 1  ]
 then
 	xrandr --output HDMI-0 --auto --rotate normal --dpi 96 --rate 60 --output eDP-1-1 --primary --mode 1920x1080 --rotate normal --left-of HDMI-0 --dpi 96 --rate 60
 else
-	xrandr --output eDP-1-1 --primary --mode 1920x1080 --rotate normal --dpi 96 --rate 60
+	if [ $virtual1 -eq 1 ] then
+	   xrandr --output Virtual-1 --primary --mode 1920x1080 --rotate normal --dpi 96 --rate 60
+	else
+	   xrandr --output eDP-1-1 --primary --mode 1920x1080 --rotate normal --dpi 96 --rate 60
+	fi
 fi
