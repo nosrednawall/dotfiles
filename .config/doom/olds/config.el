79aead00a3835d70b2a56f86aad6ccf6a5256cb6
nosrednawall <nosrednawall@gmail.com>
2025-08-01 10:08:53 -0300

    backup

--- .config/doom/olds/config.el
+++ .config/doom/olds/config.el
@@ -9,6 +9,9 @@
 ;; (setq user-full-name "John Doe"
 ;;       user-mail-address "john@doe.com")
 
+(setq user-full-name "Anderson Inácio"
+      user-mail-address "anderson.inacio.dev@gmail.com")
+
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
 ;; - `doom-font' -- the primary font to use
@@ -23,7 +26,10 @@
 ;;
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
+
+;;(setq doom-font (font-spec :family "CaskaydiaMono Nerd Font" :size 16 :weight 'regular)
+;;      doom-variable-pitch-font (font-spec :family "CaskaydiaMono Nerd Font" :size 16))
+
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
@@ -32,7 +38,16 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-solarized-dark)
+;;(setq doom-theme 'doom-gruvbox)
+
+(setq doom-theme 'doom-gruvbox)
+
+;;(setq doom-font (font-spec :family "FiraMono Nerd Font Mono" :size 15))
+;;(setq doom-font (font-spec :family "Fira Mono" :size 15))
+(setq doom-font (font-spec :family "Iosevka" :size 15))
+
+(setq fancy-splash-image
+      (concat doom-user-dir "doom-banners/splashes/doom/doom-emacs-color2.png"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -74,7 +89,75 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(defun indent-according-to-mode nil)
+(electric-indent-mode -1)
+
+;; Neotree
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
 ;; Adiciona navegação entre os buffers, com as teclas Alt+Setas
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
+;; Desativar formatação automática ao salvar
+(setq +format-on-save-enabled nil)
+
+;; Copy paste term emacs
+(defun my-term-mode-hook ()
+  (define-key term-raw-map (kbd "C-y") 'term-paste)
+  (define-key term-raw-map (kbd "C-k")
+              (lambda ()
+                (interactive)
+                (term-send-raw-string "\C-k")
+                (kill-line))))
+(add-hook 'term-mode-hook 'my-term-mode-hook)
+
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+;; Company config for ess
+
+(setq company-selection-wrap-around t
+      company-tooltip-align-annotations t
+      company-idle-delay 0.45
+      company-minimum-prefix-length 3
+      company-tooltip-limit 10)
+
+;; FOr quarto mode
+(add-to-list 'auto-mode-alist '("\\.Rmd\\'" . poly-quarto-mode))
+
+;; Altera o local para o diretorio de R
+(defun my/set-r-project-directory ()
+  "Set the working directory to the one containing the .Rproj file."
+  (interactive)
+  (let ((rproj (locate-dominating-file default-directory ".Rproj")))
+    (when rproj
+      (cd rproj)
+      (message "Set working directory to: %s" rproj))))
+
+(add-hook 'ess-R-post-run-hook 'my/set-r-project-directory)
+
+
+;; Navegador para ess
+(setq browse-url-emacs-program "firefox") ; ou seu navegador
+(setq browse-url-browser-function 'browse-url-generic)
+
+
+(global-unset-key (kbd "C-z"))
+
+(global-set-key (kbd "C-z C-z") 'my-suspend-frame)
+
+(defun my-suspend-frame ()
+  "In a GUI environment, do nothing; otherwise `suspend-frame'."
+  (interactive)
+  (if (display-graphic-p)
+      (message "suspend-frame disabled for graphical displays.")
+    (suspend-frame)))
+
+;; Shinybg
+(add-to-list 'load-path "/home/anderson/.config/doom/shinybg/")
+(require 'shinybg)
